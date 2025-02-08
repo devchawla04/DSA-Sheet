b4ac77e59db8bff05aed17ef001e44c80b84d219
DevChawla <119198647+DevChawla@users.noreply.github.com>
2025-02-08 15:07:58 +0530

    set matrix zeroes optimised

---
+++ Set matrix zeroes Opitimized.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size(); 
+        int n = m > 0 ? matrix[0].size() : 0;
+        int col0 = 1;
+        
+
+        // this loop creates 0 row and 0th column as indigators
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+
+                    matrix[i][0] = 0;
+
+                    if (j != 0) {
+                        matrix[0][j] = 0;
+                    } else {
+                        col0 = 0;
+                    }
+                }
+            }
+        }
+
+     // this loop changes the inside 2d array to answer
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[0][j] == 0 || matrix[i][0] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        // this converts the indigators to 0
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < n; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+         // this converts the indigators to 0
+        if (col0 == 0) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+     
+};
